7e66f40ce0df0a37a5ebf3d047ed45d3cbb52611
Vincent Bavitz <vincent@bavitz.org>
2023-09-13 01:39:40 +1000

    Refactor into utils directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,24 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { combineArray, slideArray } from "@/utils/array-helpers";
+import { useCallback, useEffect, useState } from "react";
 import Board from "../components/Board";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import NewGame from "../components/NewGame";
 
-const boardSize = 16;
-
-function slideArray(arr: number[]): number[] {
-  let numbers = arr.filter((v) => v !== 0);
-  let zeros = new Array(4 - numbers.length).fill(0);
-  return zeros.concat(numbers);
-}
-
-function combineArray(arr: number[]): { arr: number[]; combinedScore: number } {
-  let combinedScore = 0;
-  for (let i = 3; i > 0; i--) {
-    if (arr[i] === arr[i - 1] && arr[i] !== 0) {
-      arr[i] = arr[i] * 2;
-      combinedScore += arr[i];
-      arr[i - 1] = 0;
-      i--;
-    }
-  }
-  return { arr, combinedScore };
-}
+const BOARD_SIZE = 16;
 
 type Grid = number[][];
 type Direction = "UP" | "DOWN" | "LEFT" | "RIGHT";
 
+/**
+ *
+ * @look Olek this is a JSDoc comment!
+ * You can add nice formatting `here` for other devs.
+ *
+ * Some comments in this function would help readability!!! :)
+ */
 function move(
   grid: Grid,
   direction: Direction
@@ -110,9 +99,9 @@ function isMoveAvailable(grid: Grid): boolean {
 }
 
 const initializeBoard = () => {
-  let newBoard = Array.from({ length: boardSize }, () => 0);
-  let index1 = Math.floor(Math.random() * boardSize);
-  let index2 = Math.floor(Math.random() * boardSize);
+  let newBoard = Array.from({ length: BOARD_SIZE }, () => 0);
+  let index1 = Math.floor(Math.random() * BOARD_SIZE);
+  let index2 = Math.floor(Math.random() * BOARD_SIZE);
 
   newBoard[index1] = Math.random() > 0.5 ? 2 : 4;
   newBoard[index2] = Math.random() > 0.5 ? 2 : 4;
@@ -120,82 +109,80 @@ const initializeBoard = () => {
 };
 
 export default function Home() {
-  const [board, setBoard] = useState<number[]>(
-    Array.from({ length: boardSize }, () => 0)
-  );
+  const [board, setBoard] = useState<number[]>(initializeBoard());
+
   const [score, setScore] = useState<number>(0);
   const [gameOver, setGameOver] = useState<boolean>(false);
-
-  useEffect(() => {
-    setBoard(initializeBoard());
-  }, []);
 
   type Direction = "LEFT" | "UP" | "RIGHT" | "DOWN" | undefined;
   type Grid = number[][];
   type Board = number[];
 
-  const handleKeyDown = (event: KeyboardEvent): void => {
-    let direction: Direction;
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent): void => {
+      let direction: Direction;
 
-    switch (event.key) {
-      case "ArrowLeft":
-        direction = "LEFT";
-        break;
-      case "ArrowUp":
-        direction = "UP";
-        break;
-      case "ArrowRight":
-        direction = "RIGHT";
-        break;
-      case "ArrowDown":
-        direction = "DOWN";
-        break;
-      default:
-        break;
-    }
+      switch (event.key) {
+        case "ArrowLeft":
+          direction = "LEFT";
+          break;
+        case "ArrowUp":
+          direction = "UP";
+          break;
+        case "ArrowRight":
+          direction = "RIGHT";
+          break;
+        case "ArrowDown":
+          direction = "DOWN";
+          break;
+        default:
+          break;
+      }
 
-    if (direction) {
-      let oldGrid: Grid = [];
-      for (let i = 0; i < 4; i++) {
-        let row = [];
-        for (let j = 0; j < 4; j++) {
-          row.push(0);
+      if (direction) {
+        let oldGrid: Grid = [];
+        for (let i = 0; i < 4; i++) {
+          let row = [];
+          for (let j = 0; j < 4; j++) {
+            row.push(0);
+          }
+          oldGrid.push(row);
         }
-        oldGrid.push(row);
+
+        board.forEach((value, i) => {
+          oldGrid[Math.floor(i / 4)][i % 4] = value;
+        });
+
+        let moveResult = move([...oldGrid], direction);
+        let newGrid: Grid = moveResult.newGrid;
+
+        if (JSON.stringify(oldGrid) === JSON.stringify(newGrid)) {
+          return;
+        }
+
+        setScore((prevScore: number) => prevScore + moveResult.totalScore);
+
+        let flatNewGrid: Board = newGrid.flat();
+        let emptyCells: number[] = flatNewGrid.reduce((acc, value, index) => {
+          if (value === 0) acc.push(index);
+          return acc;
+        }, [] as number[]);
+
+        let isGameOver: boolean = !isMoveAvailable(newGrid);
+        if (emptyCells.length === 0 || isGameOver) {
+          setGameOver(true);
+        } else {
+          let randomCell: number =
+            emptyCells[Math.floor(Math.random() * emptyCells.length)];
+          let randomNumber: number = Math.random() < 0.9 ? 2 : 4;
+          flatNewGrid[randomCell] = randomNumber;
+          setBoard(flatNewGrid);
+          setGameOver(false);
+        }
       }
-
-      board.forEach((value, i) => {
-        oldGrid[Math.floor(i / 4)][i % 4] = value;
-      });
-
-      let moveResult = move([...oldGrid], direction);
-      let newGrid: Grid = moveResult.newGrid;
-
-      if (JSON.stringify(oldGrid) === JSON.stringify(newGrid)) {
-        return;
-      }
-
-      setScore((prevScore: number) => prevScore + moveResult.totalScore);
-
-      let flatNewGrid: Board = newGrid.flat();
-      let emptyCells: number[] = flatNewGrid.reduce((acc, value, index) => {
-        if (value === 0) acc.push(index);
-        return acc;
-      }, [] as number[]);
-
-      let isGameOver: boolean = !isMoveAvailable(newGrid);
-      if (emptyCells.length === 0 || isGameOver) {
-        setGameOver(true);
-      } else {
-        let randomCell: number =
-          emptyCells[Math.floor(Math.random() * emptyCells.length)];
-        let randomNumber: number = Math.random() < 0.9 ? 2 : 4;
-        flatNewGrid[randomCell] = randomNumber;
-        setBoard(flatNewGrid);
-        setGameOver(false);
-      }
-    }
-  };
+    },
+    [board]
+  );
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
