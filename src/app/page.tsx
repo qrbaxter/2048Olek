c08e343501273d9b8b5d2193c8c3e3ab11c2a179
qrbaxter <qrbaxter90@gmail.com>
2023-09-15 08:49:25 +1000

    fix refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,69 @@
-"use client";
+"use client"
 
-import useGameControls from "@/hooks/useGameControls";
-import useGameLogic from "@/hooks/useGameLogic";
+import React, { useState, useEffect } from 'react';
 import Board from "../components/Board";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import NewGame from "../components/NewGame";
+import { BOARD_SIZE } from '@/constants';
+import { useGameControls } from '@/hooks/useGameControls';
+import { useGameLogic } from '@/hooks/useGameLogic';
+
+const initializeBoard = () => {
+  let newBoard = Array.from({ length: BOARD_SIZE }, () => 0);
+  let index1 = Math.floor(Math.random() * BOARD_SIZE);
+  let index2 = Math.floor(Math.random() * BOARD_SIZE);
+  
+  newBoard[index1] = (Math.random() > 0.5 ? 2 : 4);
+  newBoard[index2] = (Math.random() > 0.5 ? 2 : 4);
+  return newBoard;
+}
+
+
 
 export default function Home() {
-  const { isGameOver, move, board } = useGameLogic();
+  const [board, setBoard] = useState<number[]>(Array.from({ length: BOARD_SIZE }, () => 0));
+  const [score, setScore] = useState<number>(0);
+  const [gameOver, setGameOver] = useState<boolean>(false);
+  const { handleKeyDown } = useGameControls(board, setBoard, setScore, setGameOver);
+  const { move, isMoveAvailable } = useGameLogic()
 
-  useGameControls(board, move);
+  useEffect(() => {
+    setBoard(initializeBoard());
+  }, []); 
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+  
+  
+const handleNewGame = () => {
+  setBoard(initializeBoard());
+  setGameOver(false);  
+  setScore(0)
+};
+
 
   return (
-    <main>
-      {isGameOver ? (
+  <main>
+    {
+      gameOver ? (
         <div className="flex justify-center items-center flex-col">
           <h1>GAME OVER</h1>
           <h1>BEST:</h1>
         </div>
       ) : (
         <>
-          <Header score={score} />
+          <Header score={score}/>
           <Board board={board} />
           <NewGame onNewGame={handleNewGame} />
+          
         </>
-      )}
-      <Footer />
-    </main>
-  );
-}
+      )
+    }
+    <Footer />
+  </main>
+  )}
--- src/hooks/useGameLogic.ts
+++ src/hooks/useGameLogic.ts
@@ -1,140 +1,94 @@
-import { BOARD_SIZE } from "@/constants";
-import { Direction, Grid } from "@/types";
-import { combineArray, slideArray } from "@/utils/array-helpers";
-import { useCallback, useState } from "react";
+import { slideArray, combineArray } from '@/utils/array.helpers';
+import { Grid, Direction } from '@/types';
 
-export type MoveFn = (direction: Direction) => {
-  newGrid: number[][];
-  totalScore: number;
-};
 
-const initializeBoard = () => {
-  let newBoard = Array.from({ length: BOARD_SIZE }, () => 0);
-  let index1 = Math.floor(Math.random() * BOARD_SIZE);
-  let index2 = Math.floor(Math.random() * BOARD_SIZE);
-
-  newBoard[index1] = Math.random() > 0.5 ? 2 : 4;
-  newBoard[index2] = Math.random() > 0.5 ? 2 : 4;
-
-  return newBoard;
-};
-
-/**
- * This hook handles all of the logic for 2048!
- */
-const useGameLogic = (grid: Grid) => {
-  const [board, setBoard] = useState<number[]>(initializeBoard());
-  const [score, setScore] = useState<number>(0);
-  const [isGameOver, setIsGameOver] = useState<boolean>(false);
-
-  /**
-   *
-   * @look Olek this is a JSDoc comment!
-   * You can add nice formatting `here` for other devs.
-   *
-   * Some comments in this function would help readability!!! :)
-   */
-  const move: MoveFn = useCallback(
-    (direction: Direction) => {
-      let newGrid = grid.map((row) => [...row]);
+export const useGameLogic = () => {
+    const move = (grid: Grid, direction: Direction) => {
+      let newGrid = grid.map(row => [...row]);
 
       let totalScore = 0;
 
       if (direction === "UP" || direction === "DOWN") {
         for (let col = 0; col < 4; col++) {
-          let column: number[] = [];
-          for (let row = 0; row < 4; row++) {
-            column.push(newGrid[row][col]);
-          }
-
-          if (direction === "DOWN") {
-            column = slideArray(column);
-            let result = combineArray(column);
-            column = result.arr;
-            totalScore += result.combinedScore;
-            column = slideArray(column);
-          } else {
-            column = slideArray(column.reverse());
-            let result = combineArray(column);
-            column = result.arr;
-            totalScore += result.combinedScore;
-            column = slideArray(column).reverse();
-          }
-
-          for (let row = 0; row < 4; row++) {
-            newGrid[row][col] = column[row];
-          }
-        }
-      } else {
+        let column: number[] = [];
         for (let row = 0; row < 4; row++) {
-          if (direction === "RIGHT") {
-            newGrid[row] = slideArray(newGrid[row]);
-            let result = combineArray(newGrid[row]);
-            newGrid[row] = result.arr;
-            totalScore += result.combinedScore;
-            newGrid[row] = slideArray(newGrid[row]);
-          } else {
-            newGrid[row] = slideArray(newGrid[row].reverse());
-            let result = combineArray(newGrid[row]);
-            newGrid[row] = result.arr;
-            totalScore += result.combinedScore;
-            newGrid[row] = slideArray(newGrid[row]).reverse();
+          column.push(newGrid[row][col]);
+      }
+      
+      if (direction === "DOWN") {
+        column = slideArray(column);
+        let result = combineArray(column);
+        column = result.arr;
+        totalScore += result.combinedScore;
+        column = slideArray(column);
+      } else {
+        column = slideArray(column.reverse());
+        let result = combineArray(column);
+        column = result.arr;
+        totalScore += result.combinedScore;
+        column = slideArray(column).reverse();
+      }
+      
+      for (let row = 0; row < 4; row++) {
+        newGrid[row][col] = column[row];
+      }
+    }
+  } else {
+    for (let row = 0; row < 4; row++) {
+      if (direction === "RIGHT") {
+        newGrid[row] = slideArray(newGrid[row]);
+        let result = combineArray(newGrid[row]);
+        newGrid[row] = result.arr;
+        totalScore += result.combinedScore;
+        newGrid[row] = slideArray(newGrid[row]);
+      } else {
+        newGrid[row] = slideArray(newGrid[row].reverse());
+        let result = combineArray(newGrid[row]);
+        newGrid[row] = result.arr;
+        totalScore += result.combinedScore;
+        newGrid[row] = slideArray(newGrid[row]).reverse();
+      }
+    }
+  }
+        return { newGrid, totalScore };
+      };
+
+
+
+
+      const isMoveAvailable = (grid: Grid):boolean => {
+                //zero check
+          for (let row = 0; row < 4; row++) {
+            for (let col = 0; col < 4; col++) {
+              if (grid[row][col] === 0) return true;
+            }
           }
-        }
-      }
 
-      return { newGrid, totalScore };
-    },
-    [grid]
-  );
+          //horizontal move check
+          for (let row = 0; row < 4; row++) {
+            for (let col = 0; col < 3; col++) { 
+              if (grid[row][col] === grid[row][col + 1]) return true;
+            }
+          }
 
-  const getIsMoveAvailable = useCallback(() => {
-    //zero check
-    for (let row = 0; row < 4; row++) {
-      for (let col = 0; col < 4; col++) {
-        if (grid[row][col] === 0) {
-          return true;
-        }
-      }
-    }
+          // vertical move check
+          for (let col = 0; col < 4; col++) {
+            for (let row = 0; row < 3; row++) { 
+              if (grid[row][col] === grid[row + 1][col]) return true;
+            }
+          }
 
-    //horizontal move check
-    for (let row = 0; row < 4; row++) {
-      for (let col = 0; col < 3; col++) {
-        if (grid[row][col] === grid[row][col + 1]) {
-          return true;
-        }
-      }
-    }
+          return false
+                
 
-    // vertical move check
-    for (let col = 0; col < 4; col++) {
-      for (let row = 0; row < 3; row++) {
-        if (grid[row][col] === grid[row + 1][col]) {
-          return true;
-        }
-      }
-    }
+        };
+  
 
-    return false;
-  }, [grid]);
 
-  const startNewGame = () => {
-    setBoard(initializeBoard());
-    setIsGameOver(false);
-  };
 
-  return {
-    move,
-    startNewGame,
-    getIsMoveAvailable,
-    board,
-    score,
-    setBoard,
-    setScore,
-    isGameOver,
-    setIsGameOver,
-  };
-};
 
-export default useGameLogic;
+
+
+      return {move, isMoveAvailable}
+      
+}
